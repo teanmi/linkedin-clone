1ab462f60a2beb30ab2d022f8c99ca6b2c918e73
teanmi <teanmi6@gmail.com>
2022-12-01 23:57:47 -0500

    worked on left side

--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -16,7 +16,39 @@ const Leftside = () => {
             <AddPhotoText>Add a photo</AddPhotoText>
           </a>
         </UserInfo>
+        <Widget>
+          <a>
+            <div>
+              <span>Connections</span>
+              <span>Grow your network</span>
+            </div>
+            <img src="../images/widget-icon.svg" alt="" />
+          </a>
+        </Widget>
+        <Item>
+          <span>
+            <img src="../images/item-icon.svg" alt="" />
+            My Items
+          </span>
+        </Item>
       </ArtCard>
+      <CommunityCard>
+        <a>
+          <span>Groups</span>
+        </a>
+        <a>
+          <span>
+            Events
+            <img src="../images/plus-icon.svg" alt="" />
+          </span>
+        </a>
+        <a>
+          <span>Followed Hashtags</span>
+        </a>
+        <a>
+          <span>Discover more</span>
+        </a>
+      </CommunityCard>
     </Container>
   );
 };
@@ -38,7 +70,7 @@ const ArtCard = styled.div`
 `;
 
 const UserInfo = styled.div`
-  border-bottom: 1px solid rgba(0,0,0,0.15);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
   padding: 12px 12px 16px;
   word-wrap: break-word;
   word-break: break-word;
@@ -50,7 +82,6 @@ const CardBackground = styled.div`
   background-size: 275px;
   height: 54px;
   margin: -12px -12px 0;
-  
 `;
 
 const Photo = styled.div`
@@ -82,6 +113,75 @@ const AddPhotoText = styled.div`
   font-size: 12px;
   line-height: 1.33;
   font-weight: 400;
+`;
+
+const Widget = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  padding: 12px 0;
+
+  a {
+    text-decoration: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4px 12px;
+  }
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    span {
+      font-size: 12px;
+      line-height: 1.333;
+      &:first-child {
+        color: rgba(0, 0, 0, 0.6);
+      }
+      &:nth-child(2) {
+        color: rgba(0, 0, 0, 1);
+      }
+    }
+  }
+
+  img {
+    color: rgba(0, 0, 0, 1);
+  }
+`;
+
+const Item = styled.a`
+  border-color: rgba(0, 0, 0, 0.8);
+  padding: 12px;
+  font-size: 12px;
+  display: block;
+  text-align: left;
+  span {
+    display: flex;
+    align-items: center;
+    color: rgba(0, 0, 0, 1);
+    svg {
+      color: rgba(0, 0, 0, 0.6);
+    }
+  }
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+`;
+
+const CommunityCard = styled(ArtCard)`
+  padding: 8px 0 0;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  a {
+    color: black;
+    padding: 4px 12px;
+    font-size: 12px;
+  }
 `;
 
 export default Leftside;
