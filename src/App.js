d617fd184c6cc6e3ae14eeb84cafdf04428d7c56
teanmi <teanmi6@gmail.com>
2022-12-12 19:51:57 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login.jsx";
+import Login from "./components/Login";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import { useEffect } from "react";
