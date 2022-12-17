9f69b56f764623721922b5b8ac8cb7cf1ca7905a
teanmi <teanmi6@gmail.com>
2022-12-17 18:26:51 -0500

    api key attempt 2

---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "linkedin-clone-d8538.firebaseapp.com",
+  projectId: "linkedin-clone-d8538",
+  storageBucket: "linkedin-clone-d8538.appspot.com",
+  messagingSenderId: "444425821896",
+  appId: "1:444425821896:web:b9628c2cf58c1ea61c4ffd"
+};
--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "linkedin-clone-d8538.firebaseapp.com",
-  projectId: "linkedin-clone-d8538",
-  storageBucket: "linkedin-clone-d8538.appspot.com",
-  messagingSenderId: "444425821896",
-  appId: "1:444425821896:web:b9628c2cf58c1ea61c4ffd"
-};
+import { firebaseConfig } from "./firebase.config";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
